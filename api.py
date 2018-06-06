96120347a332a605a6cfbbc4b7313825407573e7
Ben Baert <ben_b@gmx.com>
2018-06-06 11:40:33 +0200

    Scraper looks ready

---
+++ api.py
@@ -0,0 +1,21 @@
+import falcon
+
+
+class DayResource:
+    pass
+
+
+class ChannelResource:
+    pass
+
+
+class ShowResource:
+    pass
+
+
+class ActorResource:
+    pass
+
+
+class GenreResource:
+    pass
